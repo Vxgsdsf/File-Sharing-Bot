c2056b4775de5092d35e54c47198a937b5c795f5
Vxgsdsf <90978342+Vxgsdsf@users.noreply.github.com>
2021-12-04 20:41:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>Aʙᴏᴜᴛ Mᴇ\n╭──[ 🔅 Rᴇᴇʟᴏᴀᴅ Mᴇᴅɪᴀ 🔅 ]───⍟\n│\n├🔹Dᴇᴠᴇʟᴏᴘᴇʀ : <a href='tg://user?id={OWNER_ID}'>Nᴏʙᴏᴅʏ</a>\n│\n├🔹Cʜᴀɴɴᴇʟ : <a href='https://t.me/reeloadmedia'>Rᴇᴇʟᴏᴀᴅ Mᴇᴅɪᴀ</a>\n│\n├🔸Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : <a href='https://t.me/RMChats'>RMCʜᴀᴛs</a>\n│\n╰─────────[ 😎 ]────────⍟</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
